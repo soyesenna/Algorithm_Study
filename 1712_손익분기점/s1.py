0aa7d9ad08fe4e2f3b8772509cc02fb25ec17ea6
KimJooYoung <kjt915875@gmail.com>
2021-10-28 20:38:39 +0900

    21.10.28

--- 1712_손익분기점/s1.py
+++ 1712_손익분기점/s1.py
@@ -1,14 +1,14 @@
 a, b, c = map(int, input().split())
 
-cnt = a // (c - b)
 if b < c:
+    cnt = a // (c - b)
     while True:
         gen_num = a + (b * cnt)
         profit_num = c * cnt
         if gen_num < profit_num:
             break
         cnt += 1
-        print(gen_num, profit_num)
+        #print(gen_num, profit_num)
 
     print(cnt)
 else:
